cf6acdd4833011042c749d59ac54a100a609bfc4
ateffal <a.teffal@gmail.com>
2025-11-02 11:24:14 +0100

    new commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {Customers} from './customers/customers';
+import {Accounts} from './accounts/accounts';
+import {NewCustomer} from './new-customer/new-customer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path : "customers", component: Customers},
+  { path : "accounts", component: Accounts},
+  { path : "customers/search", component: Customers},
+  { path : "new-customer", component: NewCustomer},
+];
