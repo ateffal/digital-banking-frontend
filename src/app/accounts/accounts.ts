78b91d9497cf201c0c13091a4f60a016121c1a42
ateffal <a.teffal@gmail.com>
2025-11-02 21:38:45 +0100

    new commit

--- src/app/accounts/accounts.ts
+++ src/app/accounts/accounts.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-accounts',
-  imports: [],
   templateUrl: './accounts.html',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
   styleUrl: './accounts.css'
 })
-export class Accounts {
+export class Accounts implements OnInit {
 
+    accountFormGroup!: FormGroup;
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.accountFormGroup = this.fb.group({
+          accountId:this.fb.control('')
+        })
+    }
+
+
+  handleSearchAccount() {
+
+  }
 }
